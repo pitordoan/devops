fdc7bf0266ce6e9b2ff38afc7b7c81def9ff7125
Peter Doan <pitordoan@gmail.com>
2017-04-05 01:24:55 -0700

    Added get_id and added support for function arguments

--- devops.py
+++ devops.py
@@ -3,7 +3,9 @@
 import subprocess
 import sys
 import inspect
+from subprocess import check_output
 
+#Returns available disk space
 def get_avail_disk_space():
     cmd = 'df'
     p = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE)
@@ -11,12 +13,17 @@ def get_avail_disk_space():
     avail_space = output.split('\n')[1].split()[3]
     return int(int(avail_space.strip()) / 1024) #in MB
 
+#Returns free memory
 def get_free_mem():
     cmd = 'free'
     p = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE)
     output = p.communicate()[0]
     free_mem = output.split('\n')[1].split()[3]
     return int(int(free_mem.strip()) / 1024)  # in MB
+
+#Returns an array of PIDs that their names match process_name
+def get_pid(process_name):
+    return map(int, check_output(["pidof", process_name]).split())
 
 def main():
     if len(sys.argv) <= 1:
@@ -26,11 +33,17 @@ def main():
         found = False
 
         for f in fset:
-            if f.func_name == sys.argv[1]:
-                fn = f.func_name + "()"
-                result = eval(fn)
-                print result
+            fn_call = sys.argv[1]
+            if "(" in fn_call:
+                p = fn_call.index("(")
+            else:
+                p = len(fn_call)
+                fn_call = fn_call + "()"
 
+            fn_name = fn_call[0:p]
+            if f.func_name == fn_name:
+                result = eval(fn_call)
+                print result
                 found = True
                 break
 
