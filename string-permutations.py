32d395448cb6507dbab180a683bdfb620dad5d28
Peter Doan <pitordoan@gmail.com>
2017-05-05 23:15:02 -0700

    Initial commit

---
+++ string-permutations.py
@@ -0,0 +1,17 @@
+from sets import Set
+
+def find_permutation(string):
+    if len(string) <= 1:
+        return string
+
+    perms = Set()
+
+    for c in string:
+        re_perms = find_permutation(string.replace(c, '', 1))
+        for perm in re_perms:
+            perms.add(c + perm)
+
+    return list(perms)
+
+s = 'abc'
+print find_permutation(s)
